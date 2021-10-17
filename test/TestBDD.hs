f26c27c3ac4653a9635fb01e64b858cbf4d9d8a6
Masahiro Sakai <masahiro.sakai@gmail.com>
2021-10-17 11:39:53 +0900

    add test suite

---
+++ test/TestBDD.hs
@@ -0,0 +1,11 @@
+{-# OPTIONS_GHC -Wall #-}
+{-# LANGUAGE TemplateHaskell #-}
+module TestBDD (bddTestGroup) where
+
+import Test.Tasty
+import Test.Tasty.QuickCheck
+import Test.Tasty.HUnit
+import Test.Tasty.TH
+
+bddTestGroup :: TestTree
+bddTestGroup = $(testGroupGenerator)
