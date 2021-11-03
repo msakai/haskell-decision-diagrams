5408625763513c749945b6d0bcd4957579bc307f
Masahiro Sakai <masahiro.sakai@gmail.com>
2021-11-03 23:59:13 +0900

    avoid using forAllShow to work with QuickCheck <2.12

--- test/Utils.hs
+++ test/Utils.hs
@@ -1,12 +1,24 @@
 {-# LANGUAGE RankNTypes #-}
-module Utils where
+module Utils
+  ( forAllItemOrder
+  ) where
 
 import Data.Proxy
 import Test.QuickCheck
 
 import Data.DecisionDiagram.BDD.Internal.ItemOrder
 
+data SomeItemOrder
+  = AscOrder
+  | DescOrder
+  deriving (Show, Enum, Bounded)
+
+instance Arbitrary SomeItemOrder where
+  arbitrary = arbitraryBoundedEnum
+
 forAllItemOrder :: Testable prop => (forall o. ItemOrder o => Proxy o -> prop) -> Property
 forAllItemOrder k =
-  forAllShow chooseAny (\x -> if x then "AscOrder" else "DescOrder") $ \b ->
-    if b then withAscOrder k else withDescOrder k
+  forAll arbitrary $ \o ->
+    case o of
+      AscOrder -> withAscOrder k
+      DescOrder -> withDescOrder k
